49085a20cd3681c6b88369b87589561b45d35f35
FilloyLuca <filloylucapro@gmail.com>
2026-02-19 12:03:38 +0100

    footer commencer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppFooter } from './components/app-footer/app-footer';
+
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,AppFooter],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('sav-app');
+  
 }

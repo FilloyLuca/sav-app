c20b2420fc7cecb6479df94931eb6017ff40a157
FilloyLuca <filloylucapro@gmail.com>
2026-03-12 11:40:46 +0100

    .ts des recipe-page

--- src/app/pages/recipe-manager-page/recipe-manager-page.ts
+++ src/app/pages/recipe-manager-page/recipe-manager-page.ts
@@ -8,23 +8,28 @@ import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-recipe-manager-page',
   standalone: true,
-  imports: [CommonModule,FormsModule,RouterModule],
+  imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './recipe-manager-page.html',
   styleUrl: './recipe-manager-page.css'
 })
 
 export class RecipeManagerPage implements OnInit {
+
   public recettes: Recette[] = [];
+
   constructor(private recetteService: RecetteService) { }
+
   ngOnInit(): void {
     this.chargerRecettes();
   }
+  
   chargerRecettes(): void {
     this.recetteService.getRecettes().subscribe({
       next: (data) => this.recettes = data,
       error: (err) => console.error("Erreur API", err)
     });
   }
+
   supprimerRecette(id: number): void {
     if (confirm("Supprimer cette recette ?")) {
       this.recetteService.deleteRecette(id).subscribe(() =>
